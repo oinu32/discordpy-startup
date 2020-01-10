286ac9bc9352f7ea6f4433d2a7a505231a505192
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-11 02:29:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -16,6 +16,13 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@bot.command()
+async def おみくじ(ctx):
+    embed = discord.Embed(title="おみくじ", description=f"{ctx.author.mention}さんの運勢！",color=0x2ECC69)
+    embed.set_thumbnail(url=ctx.author.avatar_url)
+    embed.add_field(name="[運勢] ", value=random.choice(('姫吉','大吉', '吉' , '中吉' , '小吉' ,'凶', '大凶')), inline=False)
+    await ctx.send(embed=embed)
 
 
 bot.run(token)
