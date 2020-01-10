2f50c84b634e545a116864eab40326789a8e9f89
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-11 02:12:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,6 @@ import re
 import os
 import traceback
 import random
-
 from discord.ext import tasks
 from discord.ext import commands
 from datetime import datetime
@@ -21,6 +20,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
 @bot.command()
 async def on_message(self, message):    
     # メッセージ送信者がBotだった場合は無視する
@@ -41,7 +41,35 @@ async def on_message(self, message):
             rand_num =  random.randint(1, dim)
             lst.append(rand_num)
         await message.channel.send(lst)
-    
+
+@bot.command(aliases=["choose, random"])
+    async def ちょいす(self, ctx, *choices):
+        """ここけすな"""
+        if choices:
+            await ctx.send(random.choice(choices))
+        else:
+            await ctx.show_help()
+@commands.command()
+async def きりさめ(self, ctx):
+    kirisame = ['きりさめはJKだょ？','きりさめはJCだょ！','きりさめはJSかも？','きりっっ','きりちゃん☆彡','キリンリキ']
+    await ctx.send("<@474584379071528960>" + '\n' + random.choice(kirisame))      
+@commands.command()
+async def TENGA(self, ctx):
+    await ctx.send('(っ'"'"'-'"'"')╮ =͟͟͞╰U╯ﾌﾞｵﾝ' +"<@487986743266770945>")            
+@commands.command()
+async def ぱぱ(self, ctx):
+    await ctx.send("<@&662950715945386016>"+'\nぱぱー！')
+@commands.command()
+async def くらめん(self, ctx):
+    await ctx.send("<@&549971775828656168>")
+@commands.command()
+async def えくせ(self, ctx):
+    await ctx.send("<@419876101419040776>")
+@commands.command()
+async def りず(self, ctx):
+    await ctx.send("<@356035126909468675>")
+
+
     
 
 
