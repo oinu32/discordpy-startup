583a77f4b2aba8ed666f4ee3061f22954be4109f
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-11 02:25:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,39 +1,21 @@
-import discord
-import traceback
 from discord.ext import commands
 import os
+import traceback
 
-# 読み込むコグの名前を格納しておく。
-INITIAL_EXTENSIONS = [
-    'cogs.iroiro'
-]
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-# クラスの定義。ClientのサブクラスであるBotクラスを継承。
-class MyBot(commands.Bot):
-
-    # MyBotのコンストラクタ。
-    def __init__(self, command_prefix):
-        # スーパークラスのコンストラクタに値を渡して実行。
-        super().__init__(command_prefix)
-
-        # INITIAL_COGSに格納されている名前から、コグを読み込む。
-        # エラーが発生した場合は、エラー内容を表示。
-        for cog in INITIAL_EXTENSIONS:
-            try:
-                self.load_extension(cog)
-            except Exception:
-                traceback.print_exc()
-
-    # Botの準備完了時に呼び出されるイベント
-    async def on_ready(self):
-        
-        print('-----')
-        print(self.user.name)
-        print(self.user.id)
-        print('-----')
 
 
-# MyBotのインスタンス化及び起動処理。
-if __name__ == '__main__':
-    bot = MyBot(command_prefix='?') # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
-    bot.run(token) # Botのトークン
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
