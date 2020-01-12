7c0887e29dca77b97968f9614be7a1f0cae79cc0
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-13 01:21:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,15 +11,14 @@ from datetime import datetime
 bot = commands.Bot(command_prefix='?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-    @bot.event
-    async def on_command_error(ctx, error):
-        orig_error = getattr(error, "original", error)
-        error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-        await ctx.send(error_msg)
-
-    @bot.command()
-    async def ping(ctx):
-        await ctx.send('pong')
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
     @bot.command()
     async def おみくじ(ctx):
