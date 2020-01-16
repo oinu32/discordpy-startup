debbcd01598474778089cb07a6dc6df90a9e05b7
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-16 22:33:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,9 +8,9 @@ from discord.ext import tasks
 from discord.ext import commands
 from datetime import datetime
 
-bot = commands.Bot(command_prefix='?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+bot = commands.Bot(command_prefix='?')
 def Dice(pInputMessage):
     list = []
     string = (re.match('[1-9]{1}[D]', pInputMessage).group())
@@ -97,22 +97,65 @@ async def ねこ(ctx):
 #===========================================================================================================================
 
 @bot.event
-async def on_reaction_add(reaction,user):
-    bot.dispatch("reaction_press","add",reaction,user)
+async def on_message(message):
+    """メンバー募集 (.rect@数字)"""
+    if message.content.startswith(".rect"):
+        mcount = int(message.content[6:len(message.content)])
+        text= "あと{}人 募集中\n"
+        revmsg = text.format(mcount)
+        #friend_list 押した人のList
+        frelist = []
+        msg = await client.send_message(message.channel, revmsg)
 
-@bot.event
-async def on_reaction_remove(reaction,user):
-    bot.dispatch("reaction_press","remove",reaction,user)
+        #投票の欄
+        await client.add_reaction(msg, '\u21a9')
+        await client.add_reaction(msg, '⏫')
+        await client.pin_message(msg)
 
-@bot.command()
-async def cmd(ctx):
-    def check(event,reaction,user):
-        return str(reaction.emoji) == '\U0000270b' and not user.bot
-    while True:
-        event,reaction,user = await bot.wait_for("reaction_press",check=check)
-        if event == "add":
-            await ctx.send(f"{user.mention} がリアクションを押した")
-        elif event == "remove":
-            await ctx.send(f"{user.mention} がリアクションを消した")
+        #リアクションをチェックする
+        while len(frelist) < int(message.content[6:len(message.content)]):
+            target_reaction = await client.wait_for_reaction(message=msg)
+            #発言したユーザが同一でない場合 真
+            if target_reaction.user != msg.author:
+                #==============================================================
+                #押された絵文字が既存のものの場合 >> 左　del
+                if target_reaction.reaction.emoji == '\u21a9':
+                    #==========================================================
+                    #◀のリアクションに追加があったら反応 frelistにuser.nameがあった場合　真
+                    if target_reaction.user.name in frelist:
+                        frelist.remove(target_reaction.user.name)
+                        mcount += 1
+                        #リストから名前削除
+                        await client.edit_message(msg, text.format(mcount) +
+                                                        '\n'.join(frelist))
+                            #メッセージを書き換え
 
-bot.run(token)
+                    else:
+                        pass
+                #==============================================================
+                #押された絵文字が既存のものの場合　>> 右　add
+                elif target_reaction.reaction.emoji == '⏫':
+                    if target_reaction.user.name in frelist:
+                        pass
+
+                    else:
+                        frelist.append(target_reaction.user.name)
+                        #リストに名前追加
+                        mcount = mcount - 1
+                        await client.edit_message(msg, text.format(mcount) +
+                                                        '\n'.join(frelist))
+
+
+                elif target_reaction.reaction.emoji == '✖':
+                        await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+                        await client.unpin_message(msg)
+                        break
+                await client.remove_reaction(msg, target_reaction.reaction.emoji, target_reaction.user)
+                #ユーザーがつけたリアクションを消す※権限によってはエラー
+                #==============================================================
+        else:
+            await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+
+
+
+bot.run(token)    
