f44a8b91d26ba4388c3b7af8dfa7e1825adb766c
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-17 23:23:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -32,61 +32,53 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-@bot.command()
+#=======================遊び用====================================
 async def おみくじ(ctx):
     embed = discord.Embed(title="おみくじ", description=f"{ctx.author.mention}さんの運勢！",color=0x2ECC69)
     embed.set_thumbnail(url=ctx.author.avatar_url)
     embed.add_field(name="[運勢] ", value=random.choice(('姫吉','大吉', '吉' , '中吉' , '小吉' ,'凶', '大凶')), inline=False)
     await ctx.send(embed=embed)
-
-@bot.command()
 async def ちょいす(ctx, *choices):
     """ここけすな"""
     if choices:
         await ctx.send(random.choice(choices))
     else:
         await ctx.show_help()
-
-@bot.command()
 async def だいす(ctx, inputmsg):
     if  re.match('[1-9]{1}[D]', inputmsg):
         dice = 1
     else:
-        dice = 0
-        
+        dice = 0       
     if dice == 1:
         await ctx.send('結果'+str(Dice(inputmsg)))
-    
-@bot.command()
-async def きりさめ(ctx):
-    kirisame = ['きりさめはJKだょ？','きりさめはJCかな？','きりさめはJSヵモ？','きりっっ','きりちゃん☆彡']
-    await ctx.send("<@474584379071528960>" + '\n' + random.choice(kirisame))      
-
-@bot.command()
-async def TENGA(ctx):
-    await ctx.send('(っ'"'"'-'"'"')╮ =͟͟͞╰U╯ﾌﾞｵﾝ' +"<@487986743266770945>")            
-
+#================================呼び出し=============================
+#=ろーる
 @bot.command()
 async def ぱぱ(ctx):
     await ctx.send("<@&662950715945386016>"+'\nぱぱー！')
-
-@bot.command()
 async def くらめん(ctx):
     await ctx.send("<@&549971775828656168>")
-
-@bot.command()
+async def あさ(ctx):
+    await ctx.send("<@&667699276268306435>")
+async def ひる(ctx):
+    await ctx.send("<@&667699294404345856>")
+async def よる(ctx):
+    await ctx.send("<@&667699315363414018>")
+async def しんや(ctx):
+    await ctx.send("<@&667699342538309632>")
+    
+#=個人
+async def きりさめ(ctx):
+    kirisame = ['きりさめはJKだょ？','きりさめはJCかな？','きりさめはJSヵモ？','きりっっ','きりちゃん☆彡']
+    await ctx.send("<@474584379071528960>" + '\n' + random.choice(kirisame))      
+async def てんが(ctx):
+    await ctx.send('(っ'"'"'-'"'"')╮ =͟͟͞╰U╯ﾌﾞｵﾝ' +"<@487986743266770945>")            
 async def えくせ(ctx):
     await ctx.send("<@419876101419040776>")
-
-@bot.command()
 async def りず(ctx):
     await ctx.send("<@356035126909468675>")        
-
-@bot.command()
 async def ぽり(ctx):
     await ctx.send("<@509004043922702347>")
-    
 async def おいぬ(ctx):
     await ctx.send("<@224042826520854528>")
     
@@ -98,7 +90,7 @@ async def ねこ(ctx):
     await ctx.send(random.choice(neeko))
     #await ctx.send(file=discord.File(path +'/'+random.choice(files)))
     
-#===========================================================================================================================
+#====募集=======================================================================================================================
 
 @bot.command()
 async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
