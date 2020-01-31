14ae319b5539f33217ad028892db8f5622cbbc7e
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-01-31 22:25:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,11 +21,11 @@ def Dice(pInputMessage):
         list.append(random.randint(1,int(dice_faces)))
     return list
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
                 
 @bot.command()
 async def ping(ctx):
@@ -37,6 +37,7 @@ async def おみくじ(ctx):
     embed.set_thumbnail(url=ctx.author.avatar_url)
     embed.add_field(name="[運勢] ", value=random.choice(('姫吉','大吉', '吉' , '中吉' , '小吉' ,'凶', '大凶')), inline=False)
     await ctx.send(embed=embed)
+    
 @bot.command()
 async def ちょいす(ctx, *choices):
     """ここけすな"""
