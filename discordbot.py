86b867dae92fac60daef0da424a0c145d83dfa05
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-04-03 02:00:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,16 +10,16 @@ from discord.ext import tasks
 from discord.ext import commands
 from datetime import datetime
 
-ID_CHANNEL_README = set_input.ID_CHANNEL_README
-ID_ROLE_ASA = set_input.ID_ROLE_ASA
-ID_ROLE_HIRU = set_input.ID_ROLE_HIRU
-ID_ROLE_YORU = set_input.ID_ROLE_YORU
-ID_ROLE_SINY = set_input.ID_ROLE_SINY
-ID_ROLE_SINK = set_input.ID_ROLE_SINK
-ID_ROLE_TSKL = set_input.ID_ROLE_TSKL
-ID_chl_syuti = set_input.ID_chl_syuti
-ID_TSKIIL = set_input.ID_TSKILL
-ID_SRV = set_input.ID_SRV
+ID_CHANNEL_README = 670669933033685008 # 該当のチャンネルのID  
+ID_ROLE_ASA = 667633293319208961 # 付けたい役職のID  
+ID_ROLE_HIRU = 667633576484929546
+ID_ROLE_YORU = 667633701118672915
+ID_ROLE_SINY = 667633870572748800
+ID_chl_syuti = 670669933033685008　#周知ちゃんねるのID
+ID_ROLE_SINK = 670691815376158781　#進行ろーるID
+ID_ROLE_TSKL = 671354476044615680  #たすきる
+ID_TSKIIL = 624668843444273164 #たすきる用ちゃんねるID
+ID_SRV = 539773033724772362 #さーばーID
 
 token = os.environ['DISCORD_BOT_TOKEN']
 bot = commands.Bot(command_prefix='?')
