38c6cfdb1fdd63f02359590c6a1449e4db1b3871
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-04-03 01:05:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import re
 import os
 import random
 import asyncio
+import set_input
 from discord.ext import tasks
 from discord.ext import commands
 from datetime import datetime
@@ -66,15 +67,15 @@ async def てんが(ctx):
     await ctx.send('(っ'"'"'-'"'"')╮ =͟͟͞╰U╯ﾌﾞｵﾝ' +"<@487986743266770945>")            
 
  #====================ROLE付与==========================
-ID_CHANNEL_README = 670669933033685008 # 該当のチャンネルのID  
-ID_ROLE_ASA = 667633293319208961 # 付けたい役職のID  
-ID_ROLE_HIRU = 667633576484929546
-ID_ROLE_YORU = 667633701118672915
-ID_ROLE_SINY = 667633870572748800
-ID_chl_syuti = 670669933033685008
-ID_ROLE_SINK = 670691815376158781
-ID_ROLE_TSKL = 671354476044615680
-ID_TSKIIL = 624668843444273164
+#ID_CHANNEL_README = 670669933033685008 # 該当のチャンネルのID  
+#ID_ROLE_ASA = 667633293319208961 # 付けたい役職のID  
+#ID_ROLE_HIRU = 667633576484929546
+#ID_ROLE_YORU = 667633701118672915
+#ID_ROLE_SINY = 667633870572748800
+#ID_chl_syuti = 670669933033685008
+#ID_ROLE_SINK = 670691815376158781
+#ID_ROLE_TSKL = 671354476044615680
+#ID_TSKIIL = 624668843444273164
 @bot.event  
 async def on_raw_reaction_add(payload):  
     channel = bot.get_channel(payload.channel_id)  
