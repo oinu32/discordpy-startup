3077e191ba295bcd2b3d0d0581035400f6221cd3
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-04-07 01:53:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,6 @@ import os
 import random
 import asyncio
 import set_input
-from oauth2client.service_account import ServiceAccountCredentials
 from discord.ext import tasks
 from discord.ext import commands
 from datetime import datetime
