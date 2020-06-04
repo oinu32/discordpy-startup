cee61b74b370dc6c6f34415bd0e3dd574eb76c71
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-06-05 00:43:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -61,7 +61,7 @@ async def sayd(ctx, *, message: str):
 
 #翻訳群      
 @bot.command()
-async def kh(ctx, *, message: str):
+async def kh(ctx, *, message.clean_content: str):
     #韓国
     translator = Translator()  
     msg=translator.translate(message, src='ja' ,dest='ko')
@@ -71,7 +71,7 @@ async def kh(ctx, *, message: str):
 async def jh(ctx, *, message: str):
     #韓国→日本
     translator = Translator()  
-    msg=translator.translate(message, src='ko' ,dest='ja')
+    msg=translator.translate(message.clean_content, src='ko' ,dest='ja')
     await ctx.send(msg.text) 
 
     
