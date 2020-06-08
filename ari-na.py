7951c1590f779fc3f96dd17b6fd41503ce7fc47a
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-06-08 23:19:07 +0900

    Update ari-na.py

--- ari-na.py
+++ ari-na.py
@@ -1,1 +1,16 @@
+import discord
+import os
+import traceback
+import re
+import os
+import math
+import random
+import asyncio
+import set_input
+from googletrans import Translator
+from discord.ext import tasks
+from discord.ext import commands
+from datetime import datetime
 
+token = os.environ['DISCORD_BOT_TOKEN']
+bot = commands.Bot(command_prefix='?ari')
