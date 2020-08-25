37692c9334fdf357544c3cc7cb7575c59dcc04db
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-08-25 11:03:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,76 +26,6 @@ def Dice(pInputMessage):
         list.append(random.randint(1,int(dice_faces)))
     return list
 
-class DamageCalculator:
-    def __init__(self):
-        self.mResult = deque()
-        self.mResultTotal = 0
-
-
-    def CalcResult(self):
-        for resultTaple in self.mResult:
-            resultval = resultTaple[1]#スコアの値を取り出している
-            self.mResultTotal = self.mResultTotal + resultval;
-
-
-    def GetResult(self):
-        return self.mResult
-
-    
-    def GetResultTotal(self):
-        return self.mResultTotal
-
-            
-    def InsertResult(self, pName, pDamage):
-        self.mResult.append( (pName, pDamage) ) 
-        
-class MessageController:
-    def __init__(self):
-        self.mDmgCalc = DamageCalculator.DamageCalculator()
-
-        
-    def InputProcess(self, pInput):
-        #ここを入力切り分けの関数にした
-        if(pInput == "/count"):
-            print("カウント開始")
-            self.DamageInput()
-            self.PrintResult()
-        else:
-            print("無効")
-
-
-    def DamageInput(self):
-        #ダメージ入力のときには、複数の入力ループが必要。
-        isEnd = False
-        while (not isEnd):
-            string = input()
-            
-            #終了の処理
-            if (string == "/end"):
-                print("カウントを終了します")
-                isEnd = True
-                continue
-            
-            #結果を抽出する
-            dmgMatch = re.match(r'[0-9]+万', string)
-
-            if(dmgMatch == None):
-                print("無効なダメージ入力です")
-                continue
-            
-            #以下は有効な入力
-            dmg10e4 = re.match(r'[0-9]+', dmgMatch.group())#100万->100にする
-            print(dmg10e4.group() + "(万)をデータに追加しました")#debug用
-            self.mDmgCalc.InsertResult("Me", int(dmg10e4.group()))#ダメージ計算機に結果を追加
-
-        
-    def PrintResult(self):
-        self.mDmgCalc.CalcResult()#合計を計算
-        for resultTaple in self.mDmgCalc.GetResult():
-            print( "名前:" + resultTaple[0] + " " + "スコア:" + str(resultTaple[1]) + "万" )
-        print( "合計" + str(self.mDmgCalc.GetResultTotal()) + "万" ) 
-
-
 
 @bot.command()
 async def ping(ctx):
@@ -366,13 +296,6 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    
-    MsgCtrl = MessageController.MessageController()
-
-    print("入力をしてください:")
-    string = input()
-    MsgCtrl.InputProcess(string)
-
     #if message.content.startswith("/count"):
     #    await message.channel.send("以下からカウントします。")
     #    wait_msg = await bot.wait_for("message", check=check)
