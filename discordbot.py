fa2b33db92863c5c75a52e5f68889e1844b625c2
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-12-24 08:59:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -516,12 +516,12 @@ async def 秒数(ctx, zan,la):
 async def 秒数2(ctx, byo,zan,la):
     """?秒数　戦闘時間　残HP LAの人のダメージ　持越し秒数がでる（正確なほう）"""
     c = 110 - int(byo) * (int(zan) / int(la))
-　　await ctx.send(ctx.author.mention + '\n' +  str(math.ceil(c)) + "秒")
+    await ctx.send(ctx.author.mention + '\n' +  str(math.ceil(c)) + "秒")
 
 async def 秒数3(ctx, byo,zan,la):
     """?秒数　戦闘時間　残HP LAの人のダメージ　持越し秒数がでる（正確なほう）"""
     c = 110 - int(byo) * (int(zan) / int(la))
-　　await ctx.send(ctx.author.mention + '\n' +  str(math.ceil(c)) + "秒")
+    await ctx.send(ctx.author.mention + '\n' +  str(math.ceil(c)) + "秒")
 
 @bot.command()
 async def フル(ctx, zan):
