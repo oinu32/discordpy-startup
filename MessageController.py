4ef3d5c3f9b0f343634a9e4a6e0652c0fcbe1a25
oinu32 <59696145+oinu32@users.noreply.github.com>
2020-12-28 20:14:14 +0900

    Update MessageController.py

--- MessageController.py
+++ MessageController.py
@@ -43,7 +43,30 @@ class MessageController:
             print(dmg10e4.group() + "(万)をデータに追加しました")#debug用
             self.mDmgCalc.InsertResult("Me", int(dmg10e4.group()))#ダメージ計算機に結果を追加
 
-        
+    def DamageInput2(self):
+        #ダメージ入力のときには、複数の入力ループが必要。
+        isEnd = False            
+        while (not isEnd):
+            string = self.mInputFunc()
+            
+            #終了の処理
+            if (string == "/endw"):
+                print("カウントを終了します")
+                isEnd = True
+                continue
+            
+            #結果を抽出する
+            dmgMatch = re.match(r'[0-9]+ちん', string)
+
+            if(dmgMatch == None):
+                print("無効なダメージ入力です")
+                continue
+            
+            #以下は有効な入力
+            dmg10e5 = re.match(r'[0-9]+', dmgMatch.group())#100万->100にする
+            print(dmg10e5.group() + "(万)をデータに追加しました")#debug用
+            self.mDmgCalc.InsertResult("Me", int(dmg10e5.group()))#ダメージ計算機に結果を追加        
+            
     def PrintResult(self):
         self.mDmgCalc.CalcResult()#合計を計算
         for resultTaple in self.mDmgCalc.GetResult():
