44f8ac97cd6a0816d98b78bfa32e8d895738460d
oinu32 <59696145+oinu32@users.noreply.github.com>
2021-05-31 22:37:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,37 +29,6 @@ def Dice(pInputMessage):
         list.append(random.randint(1,int(dice_faces)))
     return list
 
-def make_party_num(self, ctx, party_num, remainder_flag='false'):
-    team = []
-    remainder = []
-    
-    # メンバーリストを取得
-    if self.set_mem(ctx) is False:
-        return self.vc_state_err
-
-    # 指定数の確認
-    if party_num > self.mem_len or party_num <= 0:
-        return '実行できません。チーム分けできる数を指定してください。(チーム数を指定しない場合は、デフォルトで2が指定されます)'
-
-    # メンバーリストをシャッフル
-    random.shuffle(self.channel_mem)
-
-    # チーム分けで余るメンバーを取得
-    if remainder_flag:
-        remainder_num = self.mem_len % party_num
-        if remainder_num != 0: 
-            for r in range(remainder_num):
-                remainder.append(self.channel_mem.pop())
-            team.append("=====余り=====")
-            team.extend(remainder)
-
-    # チーム分け
-    for i in range(party_num): 
-        team.append("=====チーム"+str(i+1)+"=====")
-        team.extend(self.channel_mem[i:self.mem_len:party_num])
-
-    return ('\n'.join(team))
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
